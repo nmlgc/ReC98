4e16a92b07991b757d1a83f8c6b62683816314ad
nmlgc <nmlgc@nmlgc.net>
2014-09-01 09:17:32 +0200

    [Reduction] #429: ctype

--- th03_mainl.asm
+++ th03_mainl.asm
@@ -12592,41 +12592,7 @@ include libs/BorlandC/__IOERROR[data].asm
 		db    0
 include libs/BorlandC/new[data].asm
 __stklen	dw 1000h
-		public __ctype
-; unsigned __int8 _ctype[257]
-__ctype		db    0, 20h, 20h, 20h,	20h, 20h, 20h, 20h
-		db  20h, 20h, 21h, 21h,	21h, 21h, 21h, 20h
-		db  20h, 20h, 20h, 20h,	20h, 20h, 20h, 20h
-		db  20h, 20h, 20h, 20h,	20h, 20h, 20h, 20h
-		db  20h, 81h, 40h, 40h,	40h, 40h, 40h, 40h
-		db  40h, 40h, 40h, 40h,	40h, 40h, 40h, 40h
-		db  40h, 12h, 12h, 12h,	12h, 12h, 12h, 12h
-		db  12h, 12h, 12h, 40h,	40h, 40h, 40h, 40h
-		db  40h, 40h, 14h, 14h,	14h, 14h, 14h, 14h
-		db    4,   4,	4,   4,	  4,   4,   4,	 4
-		db    4,   4,	4,   4,	  4,   4,   4,	 4
-		db    4,   4,	4,   4,	40h, 40h, 40h, 40h
-		db  40h, 40h, 18h, 18h,	18h, 18h, 18h, 18h
-		db    8,   8,	8,   8,	  8,   8,   8,	 8
-		db    8,   8,	8,   8,	  8,   8,   8,	 8
-		db    8,   8,	8,   8,	40h, 40h, 40h, 40h
-		db  20h,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0,   0,	0,   0,	  0,   0,   0,	 0
-		db    0
+include libs/master.lib/ctype[data].asm
 		db 0
 		db    0
 		db    0
