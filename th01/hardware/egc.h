0de07a489f8dba61327d8cadc865330137bda5bf
nmlgc <nmlgc@nmlgc.net>
2020-09-26 21:09:10 +0200

    [Maintenance] Mirror the egc_setup_copy() macro in C land
    Part of P0121, funded by Yanga.

--- th01/hardware/egc.h
+++ th01/hardware/egc.h
@@ -1,4 +1,15 @@
 #include "defconv.h"
+#include "decomp.h"
+
+// Requires the EGC to have been activated before.
+#define egc_setup_copy() \
+	OUTW2(EGC_ACTIVEPLANEREG, 0xFFF0); \
+	OUTW2(EGC_READPLANEREG, 0x00FF); \
+	/* EGC_COMPAREREAD | EGC_WS_PATREG | EGC_RL_MEMREAD */  \
+	OUTW2(EGC_MODE_ROP_REG, 0x3100);  \
+	OUTW2(EGC_MASKREG, 0xFFFF);  \
+	OUTW2(EGC_ADDRRESSREG, 0);  \
+	OUTW2(EGC_BITLENGTHREG, 0xF);
 
 // Blits the rectangle from (⌊x/16⌋*16, y) to (⌈((x + w)/16)*16⌉, (y + h))
 // from VRAM page 1 to the same position on VRAM page 0.
--- th01/hardware/egcscopy.c
+++ th01/hardware/egcscopy.c
@@ -1,13 +1,8 @@
+#include "th01/hardware/egc.h"
 #include "decomp.h"
 
 void pascal egc_start_copy(void)
 {
 	egc_on();
-	OUTW2(EGC_ACTIVEPLANEREG, 0xFFF0);
-	OUTW2(EGC_READPLANEREG, 0x00FF);
-	// EGC_COMPAREREAD | EGC_WS_PATREG | EGC_RL_MEMREAD
-	OUTW2(EGC_MODE_ROP_REG, 0x3100);
-	OUTW2(EGC_MASKREG, 0xFFFF);
-	OUTW2(EGC_ADDRRESSREG, 0);
-	OUTW2(EGC_BITLENGTHREG, 0xF);
+	egc_setup_copy();
 }
