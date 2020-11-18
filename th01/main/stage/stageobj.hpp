3fc43a3dba76311ee6be74f299d2dba5bbfad37a
nmlgc <nmlgc@nmlgc.net>
2020-11-18 12:45:22 +0100

    [Maintenance] [th01] Start a new header file for stage objects
    As we're going to see, some of these share some of the allocations.
    
    Part of P0128, funded by Yanga.

--- th01/main/stage/stageobj.hpp
+++ th01/main/stage/stageobj.hpp
@@ -1,6 +1,10 @@
+// Portals
+// -------
+
 extern screen_x_t portal_dst_left;
 extern screen_y_t portal_dst_top;
 // Additional flag on top of [orb_in_portal]. Left as true for a few more
 // frames after the orb has exited a portal, to prevent it from immediately
 // re-entering.
 extern bool16 portals_blocked;
+// -------
