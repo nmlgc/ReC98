d70db0e3dce3e26a0302c202b6977d81812efeea
nmlgc <nmlgc@nmlgc.net>
2020-12-24 13:53:10 +0100

    [Maintenance] Move .PI row pointer arithmetic to its own macros
    Part of P0132, funded by [Anonymous].

--- th02/formats/pi.h
+++ th02/formats/pi.h
@@ -6,6 +6,9 @@
 # define PI_SLOT_COUNT 6
 #endif
 
+#define PI_W RES_X
+#define PI_H RES_Y
+
 // Global PI file slots.
 extern PiHeader pi_headers[PI_SLOT_COUNT];
 extern void far *pi_buffers[PI_SLOT_COUNT];
@@ -28,3 +31,24 @@ void DEFCONV pi_put_8(screen_x_t left, vram_y_t top, int slot);
 	pi_palette_apply(slot); \
 	pi_put_8(0, 0, slot); \
 	pi_free(slot); \
+
+// .PI row pointer arithmetic
+// --------------------------
+
+typedef uint8_t far *pi_buffer_p_t;
+
+#define pi_buffer_p_init(p, slot) \
+	p = reinterpret_cast<pi_buffer_p_t>(pi_buffers[slot]);
+
+#define pi_buffer_p_offset(p, x, y) \
+	p += (((y * PI_W) + x) / 2);
+
+#define pi_buffer_p_normalize(p) \
+	p = reinterpret_cast<pi_buffer_p_t>(MK_FP( \
+		(seg_t)(reinterpret_cast<uint32_t>(p) >> 16) + (FP_OFF(p) >> 4), \
+		(FP_OFF(p) & 0xF) \
+	));
+
+#define pi_buffer_p_next_row(p, slot) \
+	pi_buffer_p_offset(p, pi_headers[slot].xsize, 0);
+// --------------------------
--- th02/formats/pi_put.cpp
+++ th02/formats/pi_put.cpp
@@ -17,19 +17,18 @@ void pi_palette_apply(int slot)
 
 void DEFCONV pi_put_8(screen_x_t left, vram_y_t top, int slot)
 {
-	char *row_p = reinterpret_cast<char *>(pi_buffers[slot]);
+	pi_buffer_p_t row_p;
 	pixel_t y;
+
+	pi_buffer_p_init(row_p, slot);
 	for(y = 0; y < pi_headers[slot].ysize; y++) {
 		graph_pack_put_8(left, top, row_p, pi_headers[slot].xsize);
 		top++;
 		if(top >= RES_Y) {
 			top -= RES_Y;
 		}
-		row_p += pi_headers[slot].xsize / 2;
-		row_p = reinterpret_cast<char far *>(MK_FP(
-			(int)((unsigned long)(row_p) >> 16) + (FP_OFF(row_p) >> 4),
-			FP_OFF(row_p) & 0xF
-		));
+		pi_buffer_p_next_row(row_p, slot);
+		pi_buffer_p_normalize(row_p);
 	}
 }
 
