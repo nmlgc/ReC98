06c4694501b93dfeff066649d6f8ef8b22728b77
nmlgc <nmlgc@nmlgc.net>
2021-05-26 21:41:17 +0200

    [Decompilation] Add inline functions to calculate playfield fractions
    Oh no, another place in which I'm going too far?! Just because I'm sick
    of hardcoded number literals after 6 years? Hm…
    
    Part of P0142, funded by Yanga.

--- th01/main/playfld.hpp
+++ th01/main/playfld.hpp
@@ -9,3 +9,25 @@ static const pixel_t PLAYFIELD_H = (PLAYFIELD_BOTTOM - PLAYFIELD_TOP);
 static const screen_x_t PLAYFIELD_CENTER_X = (
 	((PLAYFIELD_RIGHT - PLAYFIELD_LEFT) / 2) + PLAYFIELD_LEFT
 );
+
+inline pixel_t playfield_fraction_x(float fraction = 1.0f) {
+	return ((int)(PLAYFIELD_W * fraction));
+}
+
+static inline pixel_t playfield_fraction_y(float fraction = 1.0f) {
+	return ((int)(PLAYFIELD_H * fraction));
+}
+
+#ifdef rand
+// Calculates a random X value that spans the given [fraction] of the
+// playfield.
+static inline pixel_t playfield_rand_x(float fraction = 1.0f) {
+	return (rand() % playfield_fraction_x(fraction));
+}
+
+// Calculates a random Y value that spans the given [fraction] of the
+// playfield.
+static inline pixel_t playfield_rand_y(float fraction = 1.0f) {
+	return (rand() % playfield_fraction_y(fraction));
+}
+#endif
