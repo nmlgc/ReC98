a99cda80fd7d3a3152dccdeaa2e7f2f2d892bde3
nmlgc <nmlgc@nmlgc.net>
2021-07-24 16:32:38 +0200

    [Maintenance] Point out that player_invincible_via_bomb is exclusive to TH02
    Will make it easier to comprehend that the bullet update function is
    actually correct with regards to only looking at a single variable.
    (Yes, I know, hard to believe!)
    
    Part of P0149, funded by Blue Bolt, Ember2528, and -Tom-.

--- th02/main/player/player.h
+++ th02/main/player/player.h
@@ -2,9 +2,11 @@
 #define PLAYER_H 48
 
 // Grants invincibility when >0. Decrements by 1 each frame in that case.
-// Works independently from [player_invincible_via_bomb].
+// In TH02, this works independently from [player_invincible_via_bomb].
 extern unsigned char player_invincibility_time;
 
-// Grants invincibility as long as it's true. Works independently from
-// [player_invincibility_time].
-extern bool player_invincible_via_bomb;
+#if (GAME == 2)
+	// Grants invincibility as long as it's true. Works independently from
+	// [player_invincibility_time].
+	extern bool player_invincible_via_bomb;
+#endif
