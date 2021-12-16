e95ff108e3c73c92b924a9e4a6fbe3b0607ed4d5
nmlgc <nmlgc@nmlgc.net>
2021-12-16 13:20:52 +0100

    [Maintenance] [th01] Remove no longer used score declarations in ASM land
    Part of P0172, funded by [Anonymous] and Blue Bolt.

--- th01/hiscore/hiscore[bss].asm
+++ th01/hiscore/hiscore[bss].asm
@@ -1,28 +1,6 @@
-SCOREDAT_PLACES = 10
-SCOREDAT_NAME_KANJI = 8
-SCOREDAT_NAME_BYTES = (SCOREDAT_NAME_KANJI * 2)
-
 SCOREDAT_CLEARED = 40
 SCOREDAT_CLEARED_MAKAI = (SCOREDAT_CLEARED + 10)
 SCOREDAT_CLEARED_JIGOKU = (SCOREDAT_CLEARED + 20)
-
-SCOREDAT_NAME_KEY = 9Ch
-
-scoredat_names_t struc
-	dw SCOREDAT_PLACES dup (SCOREDAT_NAME_KANJI dup (?))
-scoredat_names_t ends
-
-scoredat_points_t struc
-	dd SCOREDAT_PLACES dup (?)
-scoredat_points_t ends
-
-scoredat_stages_t struc
-	dw SCOREDAT_PLACES dup (?)
-scoredat_stages_t ends
-
-scoredat_routes_t struc
-	dw SCOREDAT_PLACES dup (?)
-scoredat_routes_t ends
 
 public _scoredat_names, _scoredat_routes, _scoredat_stages, _scoredat_points
 public _entered_name_left, _entered_name_top
