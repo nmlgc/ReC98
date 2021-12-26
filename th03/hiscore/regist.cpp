27f901c48d8f72f431f0d19bd9dd6064ea052537
nmlgc <nmlgc@nmlgc.net>
2021-12-26 04:13:09 +0100

    [Decompilation] [th03] High score menu: Name entering loop
    Not jank-free by far, but thankfully much better than TH01's.
    
    Completes P0173, funded by [Anonymous].

--- th03/hiscore/regist.cpp
+++ th03/hiscore/regist.cpp
@@ -8,6 +8,7 @@ extern "C" {
 #include "master.hpp"
 #include "th01/rank.h"
 #include "th01/hardware/grppsafx.h"
+#include "th02/hardware/frmdelay.h"
 #include "th03/common.h"
 #include "th03/score.h"
 #include "th03/playchar.hpp"
@@ -15,6 +16,7 @@ extern "C" {
 #include "th03/sprites/regi.h"
 #include "th03/formats/cdg.h"
 #include "th03/formats/pi.hpp"
+#include "th03/hardware/input.h"
 }
 #include "th03/shiftjis/regist.hpp"
 #include "th03/formats/scoredat.hpp"
@@ -40,6 +42,12 @@ static const pixel_t NAME_SPACING = 24;
 static const pixel_t SCORE_SPACING = 16;
 static const pixel_t CELL_PADDING_X = 16;
 static const pixel_t PLACE_NUMBER_PADDED_W = (REGI_GLYPH_W + CELL_PADDING_X);
+
+inline screen_x_t place_left(const unsigned char& name_cursor_backwards) {
+	return (TABLE_LEFT + PLACE_NUMBER_PADDED_W +
+		(((SCOREDAT_NAME_LEN - 1) - name_cursor_backwards) * NAME_SPACING)
+	);
+}
 
 inline screen_y_t place_top(int place) {
 	return (TABLE_TOP + (place * TABLE_ROW_SPACING));
@@ -303,4 +311,159 @@ void near regist_rows_put(void)
 void pascal near regist_row_put(int place)
 {
 	regist_row_put_at(TABLE_LEFT, place_top(place), place);
+}
+
+void near regist_name_enter(void)
+{
+	#define cursor_blank(cursor_backwards, top) { \
+		regi_unput(place_left(cursor_backwards), top); \
+		hi.score.name[entered_place][cursor_backwards] = REGI_ASCII(' '); \
+	}
+
+	#define cursor_backspace(cursor_backwards, top) { \
+		cursor_blank(cursor_backwards, top); \
+		if(cursor_backwards < (SCOREDAT_NAME_LEN - 1)) { \
+			cursor_backwards++; \
+		} \
+	}
+
+	#define on_direction(condition, hold_frames, func) { \
+		if(condition) { \
+			if( \
+				(hold_frames == 0) || \
+				((hold_frames >= 30) && ((hold_frames % 4) == 0)) \
+			) { \
+				func \
+			} \
+			hold_frames++; \
+		} else { \
+			hold_frames = 0; \
+		} \
+	}
+
+	#define on_action(condition, lock, func) { \
+		if(condition) { \
+			if(lock == false) { \
+				func \
+			} \
+			lock = true; \
+		} else { \
+			lock = false; \
+		} \
+	}
+
+	struct input_hold_frames_t {
+		int up;
+		int down;
+		int left;
+		int right;
+	};
+	extern const input_hold_frames_t REGIST_INPUT_HOLD_INIT;
+
+	screen_y_t top;
+	int regi = REGI_A; // regi_patnum_t
+	input_hold_frames_t hold_frames = REGIST_INPUT_HOLD_INIT;
+	bool done = false;
+	bool rerender = true;
+	bool enter_locked = false;
+	unsigned char cursor_backwards = (SCOREDAT_NAME_LEN - 1);
+	bool back_locked = false;
+	top = place_top(entered_place);
+
+	while(!done) {
+		input_mode_interface();
+
+		on_direction((input_sp & INPUT_UP), hold_frames.up, {
+			if(regi != REGI_QUESTION) {
+				alphabet_putca(regi, false);
+				regi -= ALPHABET_GLYPHS_PER_ROW;
+				if(regi < 0) {
+					regi += ALPHABET_GLYPHS;
+				}
+				rerender = true;
+			}
+		});
+		on_direction((input_sp & INPUT_DOWN), hold_frames.down, {
+			if(regi != REGI_QUESTION) {
+				alphabet_putca(regi, false);
+				regi += ALPHABET_GLYPHS_PER_ROW;
+				if(regi >= ALPHABET_GLYPHS) {
+					regi -= ALPHABET_GLYPHS;
+				}
+				rerender = true;
+			}
+		});
+		on_direction((input_sp & INPUT_LEFT), hold_frames.left, {
+			alphabet_putca(regi, false);
+			if((regi % ALPHABET_GLYPHS_PER_ROW) == 0) {
+				regi += REGI_DOUBLEWIDE_X;
+			} else if((regi == REGI_BS) || (regi == REGI_SP)) {
+				regi -= 2;
+			} else {
+				regi--;
+			}
+			rerender = true;
+		});
+		on_direction((input_sp & INPUT_RIGHT), hold_frames.right, {
+			alphabet_putca(regi, false);
+			if((regi % ALPHABET_GLYPHS_PER_ROW) == REGI_DOUBLEWIDE_X) {
+				regi -= REGI_DOUBLEWIDE_X;
+			} else if((regi == REGI_M) || (regi == REGI_Z)) {
+				regi += 2;
+			} else {
+				regi++;
+			}
+			rerender = true;
+		});
+
+		on_action(
+			((input_sp & INPUT_OK) || (input_sp & INPUT_SHOT)), enter_locked, {
+				if(regi == REGI_BS) {
+					cursor_backspace(cursor_backwards, top);
+				} else if(regi == REGI_END) {
+					done = true;
+				} else {
+					hi.score.name[entered_place][cursor_backwards] =
+						static_cast<regi_patnum_t>(regi);
+					if(cursor_backwards == 0) {
+						done = true;
+					}
+					cursor_backwards--;
+				}
+				rerender = true;
+			}
+		);
+
+		on_action((input_sp & INPUT_BOMB), back_locked, { \
+			cursor_backspace(cursor_backwards, top);
+			rerender = true;
+		});
+
+		if(rerender == true) {
+			alphabet_putca(regi, true);
+
+			// Any potential glyph at this cursor position will be overwritten
+			// anyway, both on screen (by the preview) and in the name buffer
+			// (by actually selecting a character). The backspace feature also
+			// requires any previously set glyph to be erased here, due to...
+			cursor_blank(cursor_backwards, top);
+
+			// ... this full row rendering call. ZUN apparently considered it
+			// necessary because the name glyphs overlap each other due to
+			// NAME_SPACING < REGI_GLYPH_W. But even then, rendering only the
+			// previous glyph would have been enough.
+			regist_row_put(entered_place);
+
+			// Preview the selected glyph in the place row
+			if((regi % ALPHABET_GLYPHS_PER_ROW) != REGI_DOUBLEWIDE_X) {
+				regi_put(place_left(cursor_backwards), top, regi, false);
+			}
+			rerender = false;
+		}
+
+		frame_delay(1);
+	}
+
+	#undef on_action
+	#undef on_direction
 }
