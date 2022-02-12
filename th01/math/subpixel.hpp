1fd9b6a0c19ef4563b6f49f864f03ef43dbd357b
nmlgc <nmlgc@nmlgc.net>
2022-02-12 12:33:23 +0100

    [Maintenance] Introduce factor and bit count constants for subpixels
    Providing that tiny bit of additional readability.
    
    Part of P0182, funded by Lmocinemod and [Anonymous].

--- th01/math/subpixel.hpp
+++ th01/math/subpixel.hpp
@@ -7,18 +7,21 @@
 
 typedef int subpixel_t;
 
+static const subpixel_t SUBPIXEL_FACTOR = 16;
+static const char SUBPIXEL_BITS = 4;
+
 #define TO_SP(v) \
-	(v << 4)
+	(v << SUBPIXEL_BITS)
 
 #define TO_PIXEL(v) \
-	(v >> 4)
+	(v >> SUBPIXEL_BITS)
 
 // In-place conversion to a pixel. Ugly, and should not exist.
 #define TO_PIXEL_INPLACE(v) \
-	(v >>= 4)
+	(v >>= SUBPIXEL_BITS)
 
 inline subpixel_t to_sp(float pixel_v) {
-	return static_cast<subpixel_t>(pixel_v * 16.0f);
+	return static_cast<subpixel_t>(pixel_v * SUBPIXEL_FACTOR);
 }
 
 inline unsigned char to_sp8(float pixel_v) {
