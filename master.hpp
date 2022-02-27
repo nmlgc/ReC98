78dcbf4b75e5705d28edb705dd25061875b3b078
nmlgc <nmlgc@nmlgc.net>
2022-02-27 15:18:05 +0100

    [Maintenance] master.hpp transition: EGC-accelerated VRAM shift functions
    Part of P0185, funded by [Anonymous], -Tom-, and Blue Bolt.

--- master.hpp
+++ master.hpp
@@ -129,6 +129,21 @@ void MASTER_RET egc_start(void);
 // TODO: Document and add helpful macros for the EGC raster ops
 #define egc_setrop(mode_rop) \
 	outport(EGC_MODE_ROP_REG, mode_rop)
+
+#ifdef PC98_H
+	void MASTER_RET egc_shift_down(
+		screen_x_t x1, vram_y_t y1, screen_x_t x2, vram_y_t y2, pixel_t dots
+	);
+	void MASTER_RET egc_shift_left(
+		screen_x_t x1, vram_y_t y1, screen_x_t x2, vram_y_t y2, pixel_t dots
+	);
+	void MASTER_RET egc_shift_right(
+		screen_x_t x1, vram_y_t y1, screen_x_t x2, vram_y_t y2, pixel_t dots
+	);
+	void MASTER_RET egc_shift_up(
+		screen_x_t x1, vram_y_t y1, screen_x_t x2, vram_y_t y2, pixel_t dots
+	);
+#endif
 // ---
 
 // EMS
