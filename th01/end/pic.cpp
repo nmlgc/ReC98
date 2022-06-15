5bd9a27aff9750fec9a3a763a362f9184614cb4b
nmlgc <nmlgc@nmlgc.net>
2022-06-15 02:01:23 +0200

    [Mod] [th01] Optimize end_pic_show()
    Speeding up to almost exactly 50% of the original version's run time,
    just by throwing out unnecessary code and using pseudoregisters. Not
    that it helps much with the flickering, though – using the EGC is a bad
    idea here.

--- th01/end/pic.cpp
+++ th01/end/pic.cpp
@@ -1,5 +1,6 @@
-#pragma option -O-
+#pragma option -O -Z -3 -G
 
+#include "x86real.h"
 #include "th01/end/pic.hpp"
 
 void pascal end_pics_load_palette_show(const char *fn)
@@ -18,34 +19,28 @@ void end_pic_show(int quarter)
 
 	pixel_t src_left = ((quarter % 2) * PIC_W);
 	pixel_t src_top  = ((quarter / 2) * PIC_H);
-	uvram_offset_t vram_offset_src = vram_offset_shift(src_left, src_top);
-	uvram_offset_t vram_offset_dst = vram_offset_shift(PIC_LEFT, PIC_TOP);
+	uvram_offset_t vo_src = vram_offset_shift(src_left, src_top);
+	uvram_offset_t vo_dst = vram_offset_shift(PIC_LEFT, PIC_TOP);
 	vram_word_amount_t vram_x;
 	pixel_t y;
 
-	// This EGC-"accelerated" copy operation ends up performing a total of
-	// ((320 / 16) × 200 × 2) = 8000 VRAM page switches, which are everything
-	// but instant. Even the optimal assembly instructions for a *single* page
-	// switch, `MOV AL, (0|1)` followed by `OUT 0xA6, AL`, take 12 cycles on a
-	// 386 and 17 cycles on a 486, and ZUN adds the bloat of a standard
-	// function call on top of even that.
-	// Optimizations aside, using the EGC can't give you a better algorithm,
-	// as its tile egisters are limited to 16 dots. Expanding to at least 32
-	// dots would have really been nice for ≥386 CPUs...
+	// Mod: Remove unnecessary operations and use pseudoregisters to speed up
+	// the loop to 50% of the run time of the original version.
+	_ES = SEG_PLANE_B;
 	for(y = 0; y < PIC_H; y++) {
-		for(vram_x = 0; vram_x < (PIC_VRAM_W / sizeof(dots16_t)); vram_x++) {
-			dots16_t d;
+		_CX = (PIC_VRAM_W / sizeof(dots16_t));
+		do {
+			outportb2(0xA6, 1);
+			_AX = *reinterpret_cast<dots16_t far *>(MK_FP(_ES, vo_src));
+			outportb2(0xA6, 0);
+			*reinterpret_cast<dots16_t far *>(MK_FP(_ES, vo_dst)) = _AX;
 
-			graph_accesspage_func(1); egc_snap(d, vram_offset_src, 16);
-			graph_accesspage_func(0); egc_put(vram_offset_dst, d, 16);
-
-			vram_offset_src += sizeof(dots16_t);
-			vram_offset_dst += sizeof(dots16_t);
-		}
-		vram_offset_src += (ROW_SIZE - PIC_VRAM_W);
-		vram_offset_dst += (ROW_SIZE - PIC_VRAM_W);
+			vo_src += sizeof(dots16_t);
+			vo_dst += sizeof(dots16_t);
+			_CX--;
+		} while(!FLAGS_ZERO);
+		vo_src += (ROW_SIZE - PIC_VRAM_W);
+		vo_dst += (ROW_SIZE - PIC_VRAM_W);
 	}
 	egc_off();
 }
-
-#pragma option -O.
