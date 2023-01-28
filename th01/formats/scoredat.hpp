b1487295e8f04b972bbb84235756c4cda542466b
nmlgc <nmlgc@nmlgc.net>
2023-01-28 04:34:33 +0100

    [Maintenance] [th01] REYHI*.DAT: Fix parameter type of scoredat_name_get()
    Part of P0230, funded by [Anonymous].

--- th01/formats/scoredat.hpp
+++ th01/formats/scoredat.hpp
@@ -44,4 +44,4 @@ void scoredat_load_hiscore();
 
 // Sets [str] to the null-terminated name at the given [place] for the
 // difficulty previously loaded by scoredat_load().
-void scoredat_name_get(int place, char str[SCOREDAT_NAME_BYTES + 1]);
+void scoredat_name_get(int place, unsigned char str[SCOREDAT_NAME_BYTES + 1]);
