94df9a62b20780624a842036bffa6171636d52d6
nmlgc <nmlgc@nmlgc.net>
2023-01-31 22:39:42 +0100

    [Debloating] [th01] Input: Merge REIIDEN.EXE and FUUIN.EXE implementations
    While OP.EXE is completely different, it also uses distinct function
    names and therefore won't cause any issues when merging binaries. We
    still want to compile separate translation units in this commit though:
    The direct dependency of the REIIDEN.EXE implementation on debug
    functionality would otherwise force us to link all the debugged code
    into FUUIN.EXE as well.
    
    Part of P0230, funded by [Anonymous].

--- th01/main_01.cpp
+++ th01/main_01.cpp
@@ -15,7 +15,6 @@
 #include "pc98.h"
 #include "planar.h"
 #include "master.hpp"
-#include "pc98kbd.h"
 #include "shiftjis.hpp"
 #include "th01/rank.h"
 #include "th01/resident.hpp"
@@ -28,6 +27,7 @@
 #include "th01/hardware/frmdelay.h"
 #include "th01/hardware/graph.h"
 #include "th01/hardware/grp_text.hpp"
+#include "th01/hardware/input.hpp"
 #include "th01/hardware/palette.h"
 #include "th01/hardware/text.h"
 #include "th01/hardware/tram_x16.hpp"
@@ -80,8 +80,6 @@ unsigned long frame_rand;
 uscore_t score = 0;
 // ------------------------------------------------------------
 
-#include "th01/hardware/input_mf.cpp"
-
 struct {
 	// Specifies whether PTN_SLOT_STG contains the full set of sprites required
 	// for card-flipping stages (`false`), or the trimmed-down version for boss
@@ -105,119 +103,6 @@ struct {
 	}
 } ptn_slot_stg = { false };
 
-inline void bomb_doubletap_update(uint8_t& pressed, uint8_t& other) {
-	if(bomb_doubletap_frames < BOMB_DOUBLETAP_WINDOW) {
-		pressed++;
-	} else {
-		bomb_doubletap_frames = 0;
-		pressed = 1;
-		other = 0;
-	}
-}
-
-void input_sense(bool16 reset_repeat)
-{
-	static uint8_t input_prev[16];
-	int group_1, group_2, group_3, group_4;
-
-	if(reset_repeat == true) {
-		input_prev[0] = 0;
-		input_prev[1] = 0;
-		input_prev[2] = 0;
-		input_prev[3] = 0;
-		input_prev[8] = 0;
-		input_prev[9] = 0;
-		input_prev[10] = 0;
-		input_prev[11] = 0;
-		input_prev[4] = 0;
-		input_prev[5] = 0;
-		input_prev[6] = 0;
-		input_prev[7] = 0;
-		input_prev[12] = 0;
-		input_prev[13] = 0;
-		// Yup, no reset for 14 or 15.
-		input_bomb = 0;
-		return;
-	}
-	#define bomb_doubletap_shot input_prev[12]
-	#define bomb_doubletap_strike input_prev[13]
-
-	group_1 = key_sense(7);
-	group_2 = key_sense(5);
-	group_3 = key_sense(8);
-	group_4 = key_sense(9);
-	group_1 |= key_sense(7);
-	group_2 |= key_sense(5);
-	group_3 |= key_sense(8);
-	group_4 |= key_sense(9);
-
-	input_onchange_bool_2(0, 8,
-		input_up, (group_1 & K7_ARROW_UP), (group_3 & K8_NUM_8)
-	);
-	input_onchange_bool_2(1, 9,
-		input_down, (group_1 & K7_ARROW_DOWN), (group_4 & K9_NUM_2)
-	);
-	input_onchange_flag_2(2, 10,
-		input_lr, INPUT_LEFT, (group_1 & K7_ARROW_LEFT), (group_3 & K8_NUM_4)
-	);
-	input_onchange_flag_2(3, 11,
-		input_lr, INPUT_RIGHT, (group_1 & K7_ARROW_RIGHT), (group_4 & K9_NUM_6)
-	);
-	input_onchange(4, (group_2 & K5_Z), {
-		input_shot = true;
-		bomb_doubletap_update(bomb_doubletap_shot, bomb_doubletap_strike);
-	} else {
-		input_shot = false;
-	});
-	input_onchange(5, (group_2 & K5_X), {
-		input_strike = true;
-		bomb_doubletap_update(bomb_doubletap_strike, bomb_doubletap_shot);
-	} else {
-		input_strike = false;
-	});
-
-	input_pause_ok_sense(6, 7, group_1, group_2);
-
-	if(
-		(bomb_doubletap_strike >= 2 && bomb_doubletap_shot >= 2) ||
-		((input_lr == INPUT_LEFT_RIGHT) && input_shot)
-	) {
-		input_bomb = true;
-		bomb_doubletap_strike = 0;
-		bomb_doubletap_shot = 0;
-	}
-
-	if(mode_test == true) {
-		group_1 = key_sense(6);
-		group_1 |= key_sense(6);
-
-		// ZUN bug: debug_mem() itself renders a sub-screen in a blocking way,
-		// and senses input after a 3-frame delay, thus recursing back into
-		// this function. Therefore, debug_mem() will also be recursively
-		// called for every 3 frames you're holding this key.
-		// [input_prev[14]], which is supposed to prevent that, isn't set
-		// until debug_mem() returns, making this variable entirely pointless.
-		input_onchange(14, (group_1 & K6_ROLL_UP), {
-			input_mem_enter = true;
-			debug_mem();
-		} else {
-			input_mem_enter = false;
-		});
-		// And since this works as intended and only sets [input_mem_leave] to
-		// true on the first non-repeated key down event, you need to actually
-		// press and release this key once for every call to debug_mem() to get
-		// back into the game - even though debug_show_game() will make it
-		// appear as if you're already back in the game.
-		input_onchange(15, (group_1 & K6_ROLL_DOWN), {
-			input_mem_leave = true;
-			debug_show_game();
-		} else {
-			input_mem_leave = false;
-		});
-	}
-}
-
-#include "th01/hardware/input_rs.cpp"
 #include "th01/hardware/tram_x16.cpp"
 
 // Largely copy-pasted from harryup_animate().
