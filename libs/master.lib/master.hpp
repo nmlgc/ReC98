f92936f9bb673b9efb59ec580423f01360143b7e
nmlgc <nmlgc@nmlgc.net>
2024-05-21 13:58:04 +0200

    [Maintenance] master.hpp: Move memory model definitions into a separate header
    Part of P0284, funded by [Anonymous] and Blue Bolt.

--- libs/master.lib/master.hpp
+++ libs/master.lib/master.hpp
@@ -9,6 +9,8 @@
 #ifndef MASTER_HPP
 #define MASTER_HPP
 
+#include "libs/master.lib/func.hpp"
+
 /// Types
 /// -----
 
@@ -19,44 +21,6 @@
 	typedef Palette<RGB8> Palette8;
 #endif
 /// -----
-
-/// Memory model definitions (adapted from master.h)
-/// ------------------------------------------------
-
-#if !defined(MASTER_NEAR) && !defined(MASTER_FAR) && !defined(MASTER_COMPACT) && !defined(MASTER_MEDIUM)
-	#if defined(__SMALL__) || defined(__TINY__) || defined(M_I86SM) || defined(M_I86TM)
-		#define MASTER_NEAR
-	#elif defined(__COMPACT__) || defined(M_I86CM)
-		#define MASTER_COMPACT
-	#elif defined(__MEDIUM__) || defined(M_I86MM)
-		#define MASTER_MEDIUM
-	#elif defined(__LARGE__) || defined(__HUGE__) || defined(M_I86LM) || defined(M_I86HM)
-		#define MASTER_FAR
-	#endif
-#endif
-
-#if defined(MASTER_NEAR)
-	#define MASTER_RET near pascal
-	#define MASTER_CRET near cdecl
-	#define MASTER_PTR near
-#elif defined(MASTER_FAR)
-	#define MASTER_RET far pascal
-	#define MASTER_CRET far cdecl
-	#define MASTER_PTR far
-#elif defined(MASTER_COMPACT)
-	#define MASTER_RET near pascal
-	#define MASTER_CRET near cdecl
-	#define MASTER_PTR far
-#elif defined(MASTER_MEDIUM)
-	#define MASTER_RET far pascal
-	#define MASTER_CRET far cdecl
-	#define MASTER_PTR near
-#endif
-
-#ifndef MASTER_RET
-	#error Memory model for master.lib could not be determined?
-#endif
-/// ------------------------------------------------
 
 /// Original functions (only contains those actually called from ZUN code)
 /// ----------------------------------------------------------------------
