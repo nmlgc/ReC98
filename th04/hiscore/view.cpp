bf98b38c7a4d408bb442344ed67196f7d7227ae9
nmlgc <nmlgc@nmlgc.net>
2024-11-27 16:32:34 +0100

    [Reverse-engineering] [th04/th05] High Score viewer: Sprite IDs
    Part of P0298, funded by [Anonymous], Blue Bolt, and Splashman.

--- th04/hiscore/view.cpp
+++ th04/hiscore/view.cpp
@@ -3,3 +3,21 @@
 
 #include "th04/formats/scoredat/recreate.cpp"
 #include "th04/hiscore/scoredat.cpp"
+
+enum hiscore_patnum_t {
+	// scnum.bft
+	// ---------
+	PAT_SCNUM = 0,
+	PAT_SCNUM_last = (PAT_SCNUM + 9),
+	#if (GAME == 5)
+		PAT_SCNUM_UNUSED,
+		PAT_SCNUM_UNUSED_last = (PAT_SCNUM_UNUSED + 9),
+	#endif
+	// ---------
+	// hi_m.bft
+	// --------
+	PAT_RANK_1,
+	PAT_RANK_2,
+	PAT_RANK_last = (PAT_RANK_1 + (RANK_COUNT * 2) - 1),
+	// --------
+};
