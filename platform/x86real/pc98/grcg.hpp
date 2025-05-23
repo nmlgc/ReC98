71eb5550c3c39998696c2504b20fbec27f308107
nmlgc <nmlgc@nmlgc.net>
2025-05-23 19:43:09 +0200

    [Platform] [PC-98] GRCG: Drop the separate static color types
    Yeah, those were silly if we consider that there are multiple places
    where the games want to switch between various statically known colors
    without turning the GRCG off. Separate inlined functions is all we need
    here.
    
    Part of P0316, funded by [Anonymous], Ember2528, and Congrio.

--- platform/x86real/pc98/grcg.hpp
+++ platform/x86real/pc98/grcg.hpp
@@ -40,41 +40,30 @@ struct GRCG {
 	void set_tile(
 		const dots8_t& b, const dots8_t& r, const dots8_t& g, const dots8_t& e
 	) {
+		set_tile_static(b, r, g, e);
+	}
+
+	void set_color(vc_t col);
+
+	// Generates the optimal instructions for setting up the GRCG with a
+	// statically known tile.
+	void set_tile_static(dots8_t b, dots8_t r, dots8_t g, dots8_t e) {
 		outportb(0x7E, b);
 		outportb(0x7E, r);
 		outportb(0x7E, g);
 		outportb(0x7E, e);
 	}
 
-	void set_color(vc_t col);
+	// Generates the optimal instructions for setting up the GRCG with a
+	// statically known color.
+	void set_color_static(vc_t col) {
+		outportb(0x7E, ((col & 0x1) ? 0xFF : 0x00));
+		outportb(0x7E, ((col & 0x2) ? 0xFF : 0x00));
+		outportb(0x7E, ((col & 0x4) ? 0xFF : 0x00));
+		outportb(0x7E, ((col & 0x8) ? 0xFF : 0x00));
+	}
 
 	~GRCG() {
 		_outportb_(0x7C, GC_OFF);
-	}
-};
-
-// Generates the optimal instructions for setting up the GRCG with a statically
-// known tile.
-template <
-	int TileB, int TileR, int TileG, int TileE
-> struct GRCGStaticTile : public GRCG {
-	GRCGStaticTile(grcg_mode_t mode, grcg_plane_mask_t plane_mask = 0) :
-		GRCG(mode, plane_mask) {
-		outportb(0x7E, TileB);
-		outportb(0x7E, TileR);
-		outportb(0x7E, TileG);
-		outportb(0x7E, TileE);
-	}
-};
-
-// Generates the optimal instructions for setting up the GRCG with a statically
-// known color.
-template <vc_t Col> struct GRCGStaticColor : public GRCG {
-	GRCGStaticColor(grcg_mode_t mode, grcg_plane_mask_t plane_mask = 0) :
-		GRCG(mode, plane_mask) {
-		outportb(0x7E, ((Col & 0x1) ? 0xFF : 0x00));
-		outportb(0x7E, ((Col & 0x2) ? 0xFF : 0x00));
-		outportb(0x7E, ((Col & 0x4) ? 0xFF : 0x00));
-		outportb(0x7E, ((Col & 0x8) ? 0xFF : 0x00));
 	}
 };
