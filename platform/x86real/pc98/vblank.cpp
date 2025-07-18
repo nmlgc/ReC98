c0744b69194c7c05dcc099358ff9f3609082fcba
nmlgc <nmlgc@nmlgc.net>
2025-07-18 03:01:57 +0200

    [Platform] [PC-98] VBLANK: Add a mechanism for running code in VBLANK
    Of course, this doesn't defend against the hardware leaving VBLANK
    while the code is still running. But it's a decent workaround for not
    delaying certain GDC mutations by a whole frame on faster systems where
    we get more cycles in VBLANK.
    
    Part of P0316, funded by [Anonymous], Ember2528, and Congrio.

--- platform/x86real/pc98/vblank.cpp
+++ platform/x86real/pc98/vblank.cpp
@@ -1,9 +1,39 @@
 #pragma option -zCPLATFORM_TEXT
 
 #include "platform/x86real/pc98/vblank.hpp"
+#include "libs/master.lib/master.hpp"
 #include "x86real.h"
+
+func_t vblank_pending;
+
+static void pascal vblank_run_pending(void)
+{
+	vblank_pending();
+	vblank_flush_func_queue();
+}
 
 bool vblank_in(void)
 {
 	return ((_inportb_(0xA0) & 0x20) ? true : false);
+}
+
+void vblank_run(func_t func)
+{
+	if(inportb(0xA0) & 0x20) {
+		func();
+		return;
+	}
+
+	// Sanity check, since we currently only support a single pending function
+	while(vblank_pending != nullptr) {
+	}
+
+	vblank_pending = func;
+	vsync_proc_set(vblank_run_pending);
+}
+
+void vblank_flush_func_queue(void)
+{
+	vblank_pending = nullptr;
+	vsync_proc_reset();
 }
--- platform/x86real/pc98/vblank.hpp
+++ platform/x86real/pc98/vblank.hpp
@@ -5,3 +5,10 @@
 
 // Returns `true` if the display is within its vertical blanking period.
 bool pascal vblank_in(void);
+
+// Runs [func] either right now if we're in VBLANK, or once after the next
+// VSync.
+void pascal vblank_run(func_t func);
+
+// Removes any pending VBLANK function previously set with vblank_run().
+void vblank_flush_func_queue(void);
