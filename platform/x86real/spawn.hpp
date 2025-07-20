093472e893bd4c2fd8b2aa5abb371780a4725cf7
nmlgc <nmlgc@nmlgc.net>
2025-07-20 20:32:28 +0200

    [Platform] [x86] Spawning: Import from the `debloated` branch
    We're either going to have all platform-specific code on `master` or
    none of it.
    
    Part of P0322, funded by [Anonymous].

---
+++ platform/x86real/spawn.hpp
@@ -0,0 +1,20 @@
+// Subprocess spawning
+// -------------------
+
+#include "platform.h"
+
+// Spawns the given executable at the top of conventional RAM, assuming that it
+// will take up the given number of bytes. Useful to keep TSRs from occupying
+// the memory area directly after the program image, which would block the C
+// heap from expanding. Returns the TSR's exit code, or -1 if memory couldn't
+// be arranged for it to run.
+// Potentially dangerous because there is no way to control the size of the
+// automatically created DOS environment segment.
+int spawn_at_top_report(
+	const uint32_t reserve_bytes, const char* path, const char *args
+);
+
+// Regular spawning immediately after this process in conventional RAM.
+// Equivalent to the standard spawn() function, but using the same API as the
+// above function for symmetry.
+int spawn_adjacent_report(const char* path, const char *args);
