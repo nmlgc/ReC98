09ab2af5c938dee570712da5b8641f0ec5f99477
nmlgc <nmlgc@nmlgc.net>
2025-07-24 10:15:54 +0200

    [Platform] [x86] Spawning: Split off environment block generation
    Very noble that I actually went for strict technical correctness in
    February 2023, but we don't actually need it. Keeping this stuff in the
    spawn code path would only add needless complications with the ZUN.COM
    TSRs we have to spawn for TH02-TH05.
    
    Part of P0322, funded by [Anonymous].

--- platform/x86real/spawn.hpp
+++ platform/x86real/spawn.hpp
@@ -3,6 +3,8 @@
 
 #include "platform.h"
 
+struct SpawnEnv;
+
 // Spawns the given executable at the top of conventional RAM, assuming that it
 // will take up the given number of bytes. Useful to keep TSRs from occupying
 // the memory area directly after the program image, which would block the C
@@ -10,11 +12,16 @@
 // be arranged for it to run.
 // Potentially dangerous because there is no way to control the size of the
 // automatically created DOS environment segment.
-int spawn_at_top_report(
-	const uint32_t reserve_bytes, const char* path, const char *args
+int pascal spawn_at_top_report(
+	const uint32_t reserve_bytes,
+	const char* path,
+	const char *args,
+	const SpawnEnv *env = nullptr
 );
 
 // Regular spawning immediately after this process in conventional RAM.
 // Equivalent to the standard spawn() function, but using the same API as the
 // above function for symmetry.
-int spawn_adjacent_report(const char* path, const char *args);
+int pascal spawn_adjacent_report(
+	const char* path, const char *args, const SpawnEnv *env = nullptr
+);
