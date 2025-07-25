9637737f0a05d9de24d4339f34aee373669aad7b
nmlgc <nmlgc@nmlgc.net>
2025-07-25 14:29:39 +0200

    [Platform] [x86] Spawning: Split space reservation from spawning
    Part of P0322, funded by [Anonymous].

--- platform/x86real/spawn.hpp
+++ platform/x86real/spawn.hpp
@@ -5,6 +5,29 @@
 
 struct SpawnEnv;
 
+static const uint16_t SPAWN_SPACER_ERROR = 0xFFFF;
+
+struct SpawnSpacer {
+	uint16_t prev_paras;
+
+	operator bool(void) const {
+		return (prev_paras == SPAWN_SPACER_ERROR);
+	}
+	void set_error(void) {
+		prev_paras = SPAWN_SPACER_ERROR;
+	}
+
+	void pascal release(void);
+};
+
+// Resizes the DOS memory block of the current process to its largest possible
+// size minus [payload_bytes], plus [process_count] times the guessed amount of
+// memory that DOS will add to new processes. Returns a spacer object that
+// evaluates to `true` on failure and a more specific error code in [errno].
+SpawnSpacer pascal spawn_claim_memory_minus(
+	uint32_t payload_bytes, unsigned int process_count, const SpawnEnv *env
+);
+
 // Spawns the given executable at the top of conventional RAM, assuming that it
 // will take up the given number of bytes. Useful to keep TSRs from occupying
 // the memory area directly after the program image, which would block the C
