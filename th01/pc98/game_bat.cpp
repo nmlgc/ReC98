72889935b26a24428652f23e3c6055805ca2d5e9
nmlgc <nmlgc@nmlgc.net>
2025-07-26 09:46:37 +0200

    [Build] [th01] Add a new subdirectory for PC-98-specific per-game code
    We can't really layer away the GAME.BAT replica or the game-specific
    blitter instantiations.
    
    Part of P0313, funded by Ember2528 and Splashman.

--- th01/pc98/game_bat.cpp
+++ th01/pc98/game_bat.cpp
@@ -1,9 +1,8 @@
 // GAME.BAT replica
 // ----------------
 
-#include "platform.h"
 #include "platform/x86real/spawn.hpp"
-#include "th01/core/entry.hpp"
+#include "th01/pc98/entry.hpp"
 #include "th01/snd/mdrv2.h"
 
 int main_setup(int argc, const char *argv[])
