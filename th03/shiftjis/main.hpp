5105e7711fab1efbe48e6d0e090f59fdaba8ad63
nmlgc <nmlgc@nmlgc.net>
2025-08-27 15:34:35 +0200

    [Landmine] [th03] Don't enforce a GDC clock frequency of 2.5MHz
    Seems to be an arbitrary limitation, as the game works just fine
    without it, even on real hardware.
    
    Part of P0313, funded by Ember2528 and Splashman.

--- th03/shiftjis/main.hpp
+++ th03/shiftjis/main.hpp
@@ -1,8 +1,2 @@
-#define ERROR_GDC_5MHZ_1 \
-	"ＧＤＣクロックが５ＭＨｚになっています。\n"
-#define ERROR_GDC_5MHZ_2 \
-	"東方夢時空は２．５ＭＨｚでないと正常にゲームがプレイ出来ません\n"
-#define ERROR_GDC_5MHZ_3 \
-	"\nお手数ですが２．５ＭＨｚに切り替えて、もう一度実行してね。\n"
 #define ERROR_OUT_OF_MEMORY \
 	"\nメモリ不足です。メモリ空きを増やしてから実行してくりゃれ\n"
