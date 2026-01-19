b6ba5b0a529edbb31efdf8c0e939263804f8ee47
nmlgc <nmlgc@nmlgc.net>
2026-01-19 21:36:33 +0100

    [Research] [th03] Discover the score reduction and extend glitches
    Longest unbroken comment block so far, at 78 lines.
    
    Part of P0335, funded by [Anonymous] and Ember2528.

--- th03/main/score.hpp
+++ th03/main/score.hpp
@@ -5,8 +5,10 @@
 static const uint8_t EXTENDS_MAX = 2;
 
 // ZUN bloat: Setting [extends_gained] to this value once it reaches
-// [EXTENDS_MAX] is a rather confusing way of preventing scores >
-// (10,000,000 × [EXTENDS_MAX]) from awarding extends.
+// [EXTENDS_MAX] is not only a rather confusing way of preventing scores >
+// (10,000,000 × [EXTENDS_MAX]) from awarding extends, but the entire reason
+// why the fireballs_hittest() landmine can cause the infamous extend glitch
+// regardless of [round_speed] once P1's score has reached 20 million.
 static const uint8_t EXTENDS_DISABLE = 255;
 
 extern score_lebcd_t score_lebcd[PLAYER_COUNT];
